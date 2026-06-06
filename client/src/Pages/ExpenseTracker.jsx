74c3e22d65e8836090e991103735fd2c49a95c35
Aashish Suryawanshi <ashishsurya2005@gmail.com>
2026-06-06 12:36:07 +0530

    fix: resolve react-hooks/exhaustive-deps ESLint warning in ExpenseTracker

--- client/src/Pages/ExpenseTracker.jsx
+++ client/src/Pages/ExpenseTracker.jsx
@@ -28,6 +28,7 @@ const ExpenseTracker = () => {
 
     useEffect(() => {
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchData = async () => {
